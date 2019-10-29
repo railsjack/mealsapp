c15da0c2af9d00bbd1eb47d02a3524b3ea9518c1
railsjack <jinnahrae@gmail.com>
2019-10-30 06:21:39 +0800

    Added Drawer

--- app/navigation/MealsNavigator.js
+++ app/navigation/MealsNavigator.js
@@ -4,6 +4,7 @@ import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { useScreens } from "react-native-screens";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -13,6 +14,7 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
+import FiltersScreen from "../screens/FiltersScreen";
 
 import Colors from "../constants/colors";
 
@@ -77,4 +79,13 @@ const MealsFavTabNavigator =
         }
       });
 
-export default createAppContainer(MealsFavTabNavigator);
+  const FiltersNavigator = createStackNavigator({
+    Filters: FiltersScreen
+  });
+
+const MainNavigator = createDrawerNavigator({
+  MealsFavs: MealsFavTabNavigator,
+  Filters: FiltersNavigator
+});
+
+export default createAppContainer(MainNavigator);
--- app/screens/CategoriesScreen.js
+++ app/screens/CategoriesScreen.js
@@ -10,6 +10,9 @@ import {
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+
 const CategoriesScreen = props => {
   const renderGridItem = itemData => (
     <CategoryGridTile
@@ -36,8 +39,21 @@ const CategoriesScreen = props => {
   );
 };
 
-CategoriesScreen.navigationOptions = {
-  headerTitle: "Meal Categories"
+CategoriesScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Meal Categories",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 export default CategoriesScreen;
