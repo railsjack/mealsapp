316f20cb858073613b96ad67610b91b95bd8a68d
railsjack <jinnahrae@gmail.com>
2019-10-29 23:30:41 +0800

    Initial commit

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const CategoriesScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+export default CategoryMealScreen;
