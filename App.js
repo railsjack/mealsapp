27956c8593eae23935469e1cab5a65cdd45e359d
railsjack <jinnahrae@gmail.com>
2019-10-30 15:00:35 +0800

    Add redux to project

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+
+// Redux part
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import promiseMiddleware from "redux-promise";
+import reducers from "./app/store/reducers";
+// Redux part - End
 
 import MealsNavigator from "./app/navigation/MealsNavigator";
 
@@ -12,7 +18,7 @@ const fetchFonts = () => {
   });
 };
 
-export default function App() {
+const App = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
 
   if (!fontLoaded) {
@@ -25,4 +31,23 @@ export default function App() {
   }
 
   return <MealsNavigator />;
-}
+};
+
+// Reduxing
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const createStoreWithRedux = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(promiseMiddleware))
+);
+
+const appRedux = () => (
+  <Provider store={createStoreWithRedux}>
+    <App />
+  </Provider>
+);
+
+export default appRedux;
---
+++ app/store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+import Meal from "./meals_reducer";
+export default combineReducers({
+  Meal
+});
